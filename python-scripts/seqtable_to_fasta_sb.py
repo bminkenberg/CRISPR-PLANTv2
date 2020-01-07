53e7af6f4d4f9f56586b310ec960e96c6a4e4f09
Bastian Minkenberg <43625129+bminkenberg@users.noreply.github.com>
2020-01-07 15:01:09 -0800

    Added missing file

---
+++ python-scripts/seqtable_to_fasta_sb.py
@@ -0,0 +1,40 @@
+# 2-13-16 I try this script to make a FASTA directly from
+# seqtable and no ID file and input FASTA necessary
+
+import sys
+import re
+
+SEQTABLE = sys.argv[1]
+OUTPUT = sys.argv[2]
+
+output = open(OUTPUT, "w")
+
+with open(SEQTABLE) as f:
+
+    for line in f:
+        line = line.strip().split(' ')
+        line = filter(lambda a: a != '', line)
+        #print >> output, line
+        if 'Sequence:' in line:
+            chrnumber = re.findall(r'\d+', line[2])
+            fasta_one = '>' + 'Chr' + chrnumber[0] + ':'
+        # only works if zero mismatches
+        if '.' in line:
+            #written to remove the NGG
+
+            if '+' == line[2]:
+                # set -3 and remove last three characters from line [5]
+                line[1] = str(int(line[1])-3)
+                line[5] = line[5][:-3]
+                fasta = fasta_one + line[0] + '-' + line[1] + '\n' + line[5]
+
+            else:
+                # set +3, add :c, and remove first three char from line[5]
+                line[5] = line[5][:-3]
+                line[0] = str(int(line[0])+3)
+                line[1] = line[1] + ':rc'
+                fasta = fasta_one + line[0] + '-' + line[1] + '\n' + line[5]
+                
+            print >> output, fasta
+        
+        
